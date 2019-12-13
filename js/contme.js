d942e7fefcbfcb9da9dd94de1715142321c58ce2
winslygeorge <57665040+winslygeorge@users.noreply.github.com>
2019-12-13 17:52:52 +0300

    Update contme.js

--- js/contme.js
+++ js/contme.js
@@ -10,10 +10,20 @@ alert("message sent successfully");
 
 }
 }
+ var name = document.getElementById("name").value();
+      var email =  document.getElementById("email").value();
+      var phone =  document.getElementById("phone").value();
+      var message =  document.getElementById("message").value();
+var param = {
+  
+  name: name,
+  email: email,
+  phone: phone,
+  message: message
+  
+}
 
-var param = []
-
-xht.open("POST", "contact", true);
+xht.open("POST", "../mail.mail.php", true);
 xht.setRequestHeader("Content-type", "application/json");
 xht.send(param);
 xht.onerror = function(){
