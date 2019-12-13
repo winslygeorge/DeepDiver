17cf8c052c3c6f8ccc0146f21c61ad606eb7c81a
winslygeorge <57665040+winslygeorge@users.noreply.github.com>
2019-12-13 19:03:35 +0300

    Update contact_me.php

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,10 +1,10 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+if(empty($_GET['name'])      ||
+   empty($_GET['email'])     ||
+   empty($_GET['phone'])     ||
+   empty($_GET['message'])   ||
+   !filter_var($_GET['email'],FILTER_VALIDATE_EMAIL))
    {
    echo "No arguments Provided!";
    return false;
