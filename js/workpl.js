b3e9ba54d31913fe35731ad32f906c0771c64582
winslygeorge <57665040+winslygeorge@users.noreply.github.com>
2019-12-13 16:33:22 +0300

    Update workpl.js

--- js/workpl.js
+++ js/workpl.js
@@ -176,10 +176,10 @@ function sendRequest(file, key) {
 	
 	
     request.send(formd);
-	 request.onprogress = function(event){
+	 /*request.onprogress = function(event){
 
           document.getElementById("ajaxpreloader").setAttribute("class", "showLoader");
-          document.getElementById("body").setAttribute("class", "hideLoader");
+        
     }
   /*  request.onloadend = function(event){
 
@@ -317,7 +317,7 @@ document.getElementById("dang").innerHTML = "<h3>"+this.responseText+"</h3>";
 	htt.addEventListener("load", loadercs);
    htt.send()
 	
-	 htt.onprogress = function(event){
+	 /*htt.onprogress = function(event){
 
           document.getElementById("ajaxpreloader").setAttribute("class", "showLoader");
           
@@ -345,7 +345,7 @@ ph.onreadystatechange = function(){
 ph.open("GET", "js/websearch2.php?index="+keyWord, true);
 	ph.addEventListener("load", loadercs);
 ph.send();
-	 ph.onprogress = function(event){
+	/* ph.onprogress = function(event){
 
           document.getElementById("ajaxpreloader").setAttribute("class", "showLoader");
           
